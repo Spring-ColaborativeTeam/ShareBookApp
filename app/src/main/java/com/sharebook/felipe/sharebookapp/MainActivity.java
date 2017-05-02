978939c611a530aafb72291b33062034f3e8bfb2
Alejandro Ramirez Garcia <wildsidde@gmail.com>
2017-05-02 17:00:36 -0500

    cambio api

--- app/src/main/java/com/sharebook/felipe/sharebookapp/MainActivity.java
+++ app/src/main/java/com/sharebook/felipe/sharebookapp/MainActivity.java
@@ -1,14 +1,15 @@
 package com.sharebook.felipe.sharebookapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -26,11 +27,30 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+//import com.facebook.HttpMethod;
+
+
+
+public class MainActivity extends AbstractAsyncActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private Button btn_loggin;
+    protected static final String TAG = MainActivity.class.getSimpleName();
 
 
     @Override
@@ -112,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
     {
         btn_loggin.setEnabled( false );
         mAuth.signInAnonymously();
+        //new FetchSecuredResourceTask().execute();
     }
     private void handleFacebookAccessToken(AccessToken token) {
         Log.d("", "handleFacebookAccessToken:" + token);
@@ -136,6 +157,70 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+        }
+
+    // ***************************************
+    // Private methods
+    // ***************************************
+    private void displayResponse(Message response) {
+        Toast.makeText(this, response.getText(), Toast.LENGTH_LONG).show();
+    }
+
+    // ***************************************
+    // Private classes
+    // ***************************************
+    private class FetchSecuredResourceTask extends AsyncTask<Void, Void, Message> {
+
+        private String username;
+
+        private String password;
+
+        @Override
+        protected void onPreExecute() {
+            showLoadingProgressDialog();
+
+            // build the message object
+            EditText editText = (EditText) findViewById(R.id.txt_user);
+            this.username = editText.getText().toString();
+
+            editText = (EditText) findViewById(R.id.txt_password);
+            this.password = editText.getText().toString();
+        }
+
+        @Override
+        protected Message doInBackground(Void... params) {
+            final String url = getString(R.string.base_uri);
+
+            // Populate the HTTP Basic Authentitcation header with the username and password
+            HttpAuthentication authHeader = new HttpBasicAuthentication(username, password);
+            HttpHeaders requestHeaders = new HttpHeaders();
+            requestHeaders.setAuthorization(authHeader);
+            requestHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            // Create a new RestTemplate instance
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+
+            try {
+                // Make the network request
+                Log.d(TAG, url);
+                ResponseEntity<Message> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<Object>(requestHeaders), Message.class);
+                return response.getBody();
+            } catch (HttpClientErrorException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+                return new Message(0, e.getStatusText(), e.getLocalizedMessage());
+            } catch (ResourceAccessException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+                return new Message(0, e.getClass().getSimpleName(), e.getLocalizedMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Message result) {
+            dismissProgressDialog();
+            displayResponse(result);
+        }
+
     }
 
 
