957d3cc5c7dfba29a73059ef58b5bf720237358a
diegomendez31 <diegomendez-4131@hotmail.com>
2017-05-02 23:53:42 -0500

    Arreglando publicar

--- app/src/main/java/com/sharebook/felipe/sharebookapp/PublicarFragment.java
+++ app/src/main/java/com/sharebook/felipe/sharebookapp/PublicarFragment.java
@@ -1,11 +1,18 @@
 package com.sharebook.felipe.sharebookapp;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,8 +22,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -27,18 +37,66 @@ import static android.app.Activity.RESULT_OK;
 public class PublicarFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_IMAGE_GALLERY = 2;
+    private static final int CAPTURE_IMAGE = 1;
+    private static final int PICK_IMAGE = 2;
     ImageView imagen;
     Button botonImagen;
     EditText mensaje;
     Button guardarBoton;
     Uri uriImagen = null;
+    String imagePath;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        botonImagen=(Button)getActivity().findViewById(R.id.buttonImagen);
+        imagen=(ImageView)getActivity().findViewById(R.id.imageView);
         return inflater.inflate(R.layout.activity_publicar, null);
     }
 
+    public void agregarFoto(View view){
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_IMAGE_GALLERY);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //uriImagen = data.getData().getPath();
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(imageBitmap);
+        }  else if (REQUEST_IMAGE_GALLERY == requestCode) {
+            Uri uri = data.getData();
+            uriImagen = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                imagen.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
