b4c7792a3eac990e2b121a330f0f14575535cd2c
Alejandro Ramirez <wildsidde@gmail.com>
2017-05-08 11:49:16 -0500

    layout solicitud

---
+++ app/src/main/java/com/sharebook/felipe/sharebookapp/SolicitudAcivity.java
@@ -0,0 +1,13 @@
+package com.sharebook.felipe.sharebookapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class SolicitudAcivity extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_solicitud_acivity);
+    }
+}
